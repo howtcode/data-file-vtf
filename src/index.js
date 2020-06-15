77f1f00dfb15eaf420a906a924200db9bb0aa726
nnmrts <nanomiratus@gmail.com>
2020-06-15 20:05:52 +0200

    added first prototype of main function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,40 @@ import {
 import Jimp from "jimp";
 // import dxt from "dxt-js";
 
+import ow from "ow";
+import is from "@sindresorhus/is";
+
 import ImageFile from "./classes/image-file.js";
 import ColorArray from "./classes/color-array.js";
+
+const converter = (input, output, options) => {
+	ow(
+		input,
+		ow.any(
+			ow.string.nonEmpty,
+			ow.dataView,
+			ow.typedArray.minLength(1),
+			ow.arrayBuffer,
+			ow.sharedArrayBuffer,
+			ow.buffer,
+			ow.object.instanceOf(URL),
+			ow.object.instanceOf(ImageData),
+			ow.object.instanceOf(ImageBitmap),
+			ow.object.instanceOf(Blob),
+			ow.object.instanceOf(File),
+			ow.object.instanceOf(FileList),
+			ow.object.instanceOf(FileReader),
+			ow.object.instanceOf(HTMLImageElement),
+			ow.object.instanceOf(HTMLVideoElement),
+			ow.object.instanceOf(HTMLPictureElement),
+			ow.object.instanceOf(HTMLSourceElement),
+			ow.object.instanceOf(HTMLCanvasElement),
+			ow.object.instanceOf(CanvasRenderingContext2D),
+			ow.object.instanceOf(SVGElement),
+			ow.object.instanceOf(ReadableStream)
+		)
+	);
+};
 
 /**
  * @param inputPath
